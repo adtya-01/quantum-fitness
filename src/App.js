4eb9d16991e8e4f81ba0acd352a34ea4d3ff2d80
adtya-01 <adithyaprdosh98@gmail.com>
2023-04-09 23:22:29 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { Box } from "@mui/material";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-import "./App.css";
-import ExerciseDetail from "./pages/ExerciseDetail";
-import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import HeroBanner from "./components/HeroBanner";
-import Exercises from "./components/Exercises";
+import './App.css';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => (
-  <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+  <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
     <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/exercise/:id" element={<ExerciseDetail />} />
     </Routes>
-    <HeroBanner />
-    <Exercises />
     <Footer />
   </Box>
 );
